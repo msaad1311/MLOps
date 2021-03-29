9f2ed8f2bac6482aa960bf7beabe1e270128dc2c
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-03-29 17:55:50 -0400

    Update pipeline.py

--- Assignments/Assignment_3/pipeline.py
+++ Assignments/Assignment_3/pipeline.py
@@ -9,4 +9,9 @@ import config
 titanic_pipe = Pipeline(
     # complete with the list of steps from the preprocessors file
     # and the list of variables from the config
+    ('categorical_imputer',pp.CategoricalImputer(config.CATEGORICAL_VARS)),
+    ('missing_indicator',pp.MissingIndicator(config.NUMERICAL_VARS))
+    ('numerical_imputer',pp.NumericalImputer(config.NUMERICAL_VARS)),
+    ('cabin_extractor',pp.ExtractFirstLetter(config.CABIN)),
+    
     )
