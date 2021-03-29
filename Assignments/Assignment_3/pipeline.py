7ec510bc55362869df8d665ea616cc86f7589f09
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-03-29 18:07:26 -0400

    updated the code to fix the errors

--- Assignments/Assignment_3/pipeline.py
+++ Assignments/Assignment_3/pipeline.py
@@ -6,15 +6,15 @@ import preprocessors as pp
 import config
 
 
-titanic_pipe = Pipeline(
+titanic_pipe = Pipeline([
     # complete with the list of steps from the preprocessors file
     # and the list of variables from the config
     ('categorical_imputer',pp.CategoricalImputer(config.CATEGORICAL_VARS)),
-    ('missing_indicator',pp.MissingIndicator(config.NUMERICAL_VARS))
+    ('missing_indicator',pp.MissingIndicator(config.NUMERICAL_VARS)),
     ('numerical_imputer',pp.NumericalImputer(config.NUMERICAL_VARS)),
     ('cabin_extractor',pp.ExtractFirstLetter(config.CABIN)),
     ('rare_labels',pp.RareLabelCategoricalEncoder(config.CATEGORICAL_VARS)),
     ('categorical_encoder',pp.CategoricalEncoder(config.CATEGORICAL_VARS)),
     ('scaler',StandardScaler()),
     ('model',LogisticRegression(C=0.0005, random_state=0))
-    )
+ ] )
