d9616ac6e2b40fe682701dbdd6748da2ea842b66
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-03-29 18:02:48 -0400

    Update train_pipeline.py

--- Assignments/Assignment_3/train_pipeline.py
+++ Assignments/Assignment_3/train_pipeline.py
@@ -13,13 +13,13 @@ def run_training():
     """Train the model."""
 
     # read training data
-
+    data = pd.read_csv('titanic.csv')
     # divide train and test
-
+    xtrain,xtest,ytrain,ytest= train_test_split(data.drop(config.TARGET,axis=1),data[config.TARGET],test_size=0.2,random_state=0)
     # fit pipeline
-
+    titanic_pipe.fit(xtrain,ytrain)
     # save pipeline
-    
+    joblib.dump(titanic_pipe,config.PIPELINE_NAME)
 
 if __name__ == '__main__':
     run_training()
